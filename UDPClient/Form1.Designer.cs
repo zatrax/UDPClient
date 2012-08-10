8279a4427b713186654819647a6fd7592e9678ee
ZaTraX <zatrax@gmail.com>
2012-08-10 15:22:47 -0400

    First Commit

---
+++ UDPClient/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace UDPClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxMessageToSend = new System.Windows.Forms.TextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxConsole = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.buttonSendMessage = new System.Windows.Forms.Button();
+            this.textBoxIpAddress = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxMessageToSend
+            // 
+            this.textBoxMessageToSend.Enabled = false;
+            this.textBoxMessageToSend.Location = new System.Drawing.Point(16, 353);
+            this.textBoxMessageToSend.Name = "textBoxMessageToSend";
+            this.textBoxMessageToSend.Size = new System.Drawing.Size(563, 20);
+            this.textBoxMessageToSend.TabIndex = 3;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(446, 13);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 2;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PORT";
+            // 
+            // textBoxConsole
+            // 
+            this.textBoxConsole.Location = new System.Drawing.Point(16, 38);
+            this.textBoxConsole.Multiline = true;
+            this.textBoxConsole.Name = "textBoxConsole";
+            this.textBoxConsole.ReadOnly = true;
+            this.textBoxConsole.Size = new System.Drawing.Size(644, 312);
+            this.textBoxConsole.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(-231, 703);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 0;
+            // 
+            // buttonSendMessage
+            // 
+            this.buttonSendMessage.Enabled = false;
+            this.buttonSendMessage.Location = new System.Drawing.Point(585, 353);
+            this.buttonSendMessage.Name = "buttonSendMessage";
+            this.buttonSendMessage.Size = new System.Drawing.Size(75, 23);
+            this.buttonSendMessage.TabIndex = 4;
+            this.buttonSendMessage.Text = "Send";
+            this.buttonSendMessage.UseVisualStyleBackColor = true;
+            this.buttonSendMessage.Click += new System.EventHandler(this.buttonSendMessage_Click);
+            // 
+            // textBoxIpAddress
+            // 
+            this.textBoxIpAddress.Location = new System.Drawing.Point(50, 10);
+            this.textBoxIpAddress.Name = "textBoxIpAddress";
+            this.textBoxIpAddress.Size = new System.Drawing.Size(155, 20);
+            this.textBoxIpAddress.TabIndex = 0;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(254, 10);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(155, 20);
+            this.textBoxPort.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 415);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSendMessage);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.textBoxConsole);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIpAddress);
+            this.Controls.Add(this.textBoxMessageToSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxMessageToSend;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxConsole;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button buttonSendMessage;
+        private System.Windows.Forms.TextBox textBoxIpAddress;
+        private System.Windows.Forms.TextBox textBoxPort;
+    }
+}
+
---
+++ UDPClient/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UDPClient
+{
+    public partial class Form1 : Form
+    {
+        private UdpClient connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            connection = new UdpClient();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (buttonConnect.Text == "Connect")
+            {
+                buttonConnect.Text = "Disconnect";
+
+                var ip = IPAddress.Parse(textBoxIpAddress.Text);
+                var port = int.Parse(textBoxPort.Text);
+                connection.Connect(ip, port);
+
+                textBoxMessageToSend.Enabled = true;
+                buttonSendMessage.Enabled = true;
+
+                var s = new UdpState { Endpoint = new IPEndPoint(ip, port), Client = connection };
+                connection.BeginReceive(MessageReceived, s);   
+            }
+            else
+            {
+                buttonConnect.Text = "Connect";
+                textBoxMessageToSend.Enabled = false;
+                buttonSendMessage.Enabled = false;
+
+                connection.Close();
+            }
+        }
+
+        private class UdpState
+        {
+            public IPEndPoint Endpoint { get; set; }
+            public UdpClient Client { get; set; }
+        }
+
+        private void MessageReceived(IAsyncResult ar)
+        {
+            var client = ((UdpState)(ar.AsyncState)).Client;
+            var endpoint = ((UdpState)(ar.AsyncState)).Endpoint;
+
+            var receiveBytes = client.EndReceive(ar, ref endpoint);
+            var receiveString = Encoding.ASCII.GetString(receiveBytes) + "\r\n";
+
+            SetText(textBoxConsole, receiveString);
+
+            var ip = IPAddress.Parse(textBoxIpAddress.Text);
+            var port = int.Parse(textBoxPort.Text);
+            var s = new UdpState { Endpoint = new IPEndPoint(ip, port), Client = connection };
+            connection.BeginReceive(MessageReceived, s);
+        }
+
+        delegate void SetTextCallback(Control ctrl, string text);
+
+        private void SetText(Control ctrl, string text)
+        {
+            if (ctrl.InvokeRequired)
+            {
+                ctrl.BeginInvoke(new SetTextCallback(SetText), ctrl, text);
+            }
+            else
+            {
+                ctrl.Text += text;
+            }
+        }
+
+        private void buttonSendMessage_Click(object sender, EventArgs e)
+        {
+            var message = textBoxMessageToSend.Text;
+            textBoxMessageToSend.Text = "";
+
+            connection.Send(Encoding.Default.GetBytes(message), message.Count());
+        }
+    }
+}
